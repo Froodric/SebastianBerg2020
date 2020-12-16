ceb77c7f944109be299b7a8e616e69a455f2bac6
Froodric <fredricjohansson294@gmail.com>
2020-12-16 19:28:56 +0100

    added mobile navbar

--- js/script.js
+++ js/script.js
@@ -22,3 +22,31 @@ const images = document.getElementsByTagName("img");
 for (let i = 0; i < images.length; i++) {
   images[i].addEventListener("contextmenu", (event) => event.preventDefault());
 }
+
+var IsOpen = false;
+
+function open_nav() {
+  document.getElementById("navbar-mobile").style.left = "-1rem";
+  document.getElementById("nav-background").style.left = "-1rem";
+  IsOpen = true;
+}
+
+function close_nav() {
+  document.getElementById("navbar-mobile").style.left = "-20rem";
+  document.getElementById("nav-background").style.left = "-20rem";
+  IsOpen = false;
+}
+
+var btn = document.getElementById("nav-button");
+
+btn.addEventListener(
+  "click",
+  function () {
+    if (IsOpen) {
+      close_nav();
+    } else {
+      open_nav();
+    }
+  },
+  false
+);
